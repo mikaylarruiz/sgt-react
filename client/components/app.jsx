a7d51a4b9cede8b4ed8da90dd5e32e2b16a4aaf6
Mikayla Ruiz <mikaylarruiz@gamil.com>
2020-04-18 07:46:02 +0000

    completed constructor and added header to the render method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
+  getGrades() {
+    fetch('/api/grades')
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }));
+  }
+
   render() {
-    return null;
+    return (
+      <header>
+        Student Grade Table
+      </header>
+    );
   }
 }
 
