f1366bbae77fa01b3376c92b345418c10f55c9bf
Mikayla Ruiz <mikaylarruiz@gamil.com>
2020-04-18 19:58:42 +0000

    Created getAverageGrade method, and added it to getGrades()

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import GradeTable from './gradeTable';
 import PageTitle from './page-title';
@@ -8,16 +9,30 @@ class App extends React.Component {
     this.state = {
       grades: []
     };
+    this.getAverageGrade = this.getAverageGrade.bind(this);
   }
 
   componentDidMount() {
     this.getGrades();
   }
 
+  getAverageGrade() {
+    var grades = this.state.grades;
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+      sum += grades[i].grade;
+    }
+    const average = sum / grades.length;
+    return Math.round(average);
+  }
+
   getGrades() {
     fetch('/api/grades')
       .then(response => response.json())
-      .then(data => this.setState({ grades: data }))
+      .then(data => {
+        this.setState({ grades: data });
+        this.getAverageGrade();
+      })
       .catch(error => console.error(error));
   }
 
--- client/components/page-title.jsx
+++ client/components/page-title.jsx
@@ -4,6 +4,7 @@ function PageTitle(props) {
   return (
     <header className="row align-items-end mt-4 mb-4">
       <h1>Student Grade Table</h1>
+      <h4 className="col text-right">Average Grade <span className="badge badge-primary"></span></h4>
     </header>
   );
 }
